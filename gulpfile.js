7e76882c337e06c3c57c056e96fb05d0ef430390
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-10-20 17:01:55 -0500

    chore(gulp): add clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 'use strict'
 
+const del = require('del')
 const gulp = require('gulp')
 const inline = require('gulp-inline')
 
 const buildDir = 'build'
 
-gulp.task('build', () =>
+gulp.task('clean', () =>
+  del(`${buildDir}`)
+)
+
+gulp.task('build', ['clean'], () =>
   gulp.src('app/index.html')
     .pipe(inline())
     .pipe(gulp.dest(buildDir))
