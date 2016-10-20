8327065ceae8fdafe19227c90594508e65c606f2
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-10-20 17:04:49 -0500

    chore(gulp): minify HTML
    Closes #2

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const csso = require('gulp-csso')
 const del = require('del')
 const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
 const inline = require('gulp-inline')
 
 const buildDir = 'build'
@@ -15,6 +16,17 @@ gulp.task('build', ['clean'], () =>
   gulp.src('app/index.html')
     .pipe(inline({
       css: csso
+    }))
+    .pipe(htmlmin({
+      collapseBooleanAttributes: true,
+      collapseInlineTagWhitespace: true,
+      collapseWhitespace: true,
+      quoteCharacter: '\'',
+      removeAttributeQuotes: true,
+      removeComments: true,
+      removeOptionalTags: true,
+      removeRedundantAttributes: true,
+      useShortDoctype: true
     }))
     .pipe(gulp.dest(buildDir))
 )
