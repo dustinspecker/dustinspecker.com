55db4167ef4b937c2c9cd005abea4cb8154d0d43
Dustin Specker <DustinSpecker@DustinSpecker.com>
2018-04-09 12:21:17 -0500

    refactor(lifts): use es2015 and jsx

--- lifts/app/index.js
+++ lifts/app/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-function App() {
-  return React.createElement('p', null, ['hello'])
-}
+const App = () =>
+  <p>hello</p>
 
-render(App(), document.querySelector('#app'))
+render(<App />, document.querySelector('#app'))
