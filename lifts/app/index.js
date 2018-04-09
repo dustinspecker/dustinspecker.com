03fec3895d6ccd8b1cc46b4b018f321fe2fad6ca
Dustin Specker <DustinSpecker@DustinSpecker.com>
2018-04-09 15:01:36 -0500

    feat(lifts): display list of lifts

--- lifts/app/index.js
+++ lifts/app/index.js
@@ -1,9 +1,20 @@
 import AppBar from 'material-ui/AppBar'
 import CssBaseline from 'material-ui/CssBaseline'
+import Lift from './components/lift'
+import List from 'material-ui/List'
+import Paper from 'material-ui/Paper'
 import React from 'react'
 import {render} from 'react-dom'
 import ToolBar from 'material-ui/ToolBar'
 import Typography from 'material-ui/Typography'
+
+const lifts = [
+  'squats',
+  'bench press',
+  'rows',
+  'overhead press',
+  'deadlifts'
+]
 
 class App extends React.Component {
   render() {
@@ -23,6 +34,22 @@ class App extends React.Component {
             </Typography>
           </ToolBar>
         </AppBar>
+        <Paper
+          elevation={0}
+        >
+          <List
+            component='nav'
+          >
+            {lifts
+              .map(lift =>
+                <Lift
+                  key={lift}
+                  name={lift}
+                />
+              )
+            }
+          </List>
+        </Paper>
       </React.Fragment>
     )
   }
