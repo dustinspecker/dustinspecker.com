4b8d8c907784d015959dbc6a68e6b8257d3e9c58
Dustin Specker <DustinSpecker@DustinSpecker.com>
2018-04-09 12:46:58 -0500

    feat(lifts); add material-ui AppBar

--- lifts/app/index.js
+++ lifts/app/index.js
@@ -1,7 +1,26 @@
+import AppBar from 'material-ui/AppBar'
+import CssBaseline from 'material-ui/CssBaseline'
 import React from 'react'
 import {render} from 'react-dom'
+import ToolBar from 'material-ui/ToolBar'
+import Typography from 'material-ui/Typography'
 
 const App = () =>
-  <p>hello</p>
+  <React.Fragment>
+    <CssBaseline />
+    <AppBar
+      color='default'
+      position='static'
+    >
+      <ToolBar>
+        <Typography
+          color='inherit'
+          variant='title'
+        >
+          Lifts
+        </Typography>
+      </ToolBar>
+    </AppBar>
+  </React.Fragment>
 
 render(<App />, document.querySelector('#app'))
