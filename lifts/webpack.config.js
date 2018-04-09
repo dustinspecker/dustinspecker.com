a337973f241685a1e469d7898c4208e23fd1f6b9
Dustin Specker <DustinSpecker@DustinSpecker.com>
2018-04-09 12:21:03 -0500

    chore(lifts): setup Babel

--- lifts/webpack.config.js
+++ lifts/webpack.config.js
@@ -8,6 +8,23 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'babel-preset-env',
+              'babel-preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       appMountId: 'app',
