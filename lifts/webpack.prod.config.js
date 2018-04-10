423968f31c63e7675b245ad029a56705fa894c06
Dustin Specker <DustinSpecker@DustinSpecker.com>
2018-04-10 15:55:09 -0500

    feat(lifts): gzip build

--- lifts/webpack.prod.config.js
+++ lifts/webpack.prod.config.js
@@ -1,8 +1,12 @@
+const CompressionPlugin = require('compression-webpack-plugin')
 const config = require('./webpack.config.js')
 const merge = require('webpack-merge')
 
 delete config.serve
 
 module.exports = merge(config, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new CompressionPlugin()
+  ]
 })
